0674563966f509ef4eda07cb4f584d105741227e
LJThao <yengfong.thao@atlasschool.com>
2024-04-13 05:09:07 +0000

    fix betty for line 4

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - function that goes through a binary tree using in-order
+ * binary_tree_inorder - function that goes through a tree using IO
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  * Return: if NULL, do nothing
