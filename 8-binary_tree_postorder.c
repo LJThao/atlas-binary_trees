9401c442126207d9797d8e10dd7296f4647d2baf
LJThao <yengfong.thao@atlasschool.com>
2024-04-13 05:07:20 +0000

    fix betty for line 4

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - function that goes through a binary tree using post-order
+ * binary_tree_postorder - function that goes through the tree using PO
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  * Return: NULL, do nothing
