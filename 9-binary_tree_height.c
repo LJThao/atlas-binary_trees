14387113db0764774a6088c7cc1cb10822f6e0ff
LJThao <yengfong.thao@atlasschool.com>
2024-04-13 05:05:14 +0000

    fix betty for line 15

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
-	return (height(binary_tree_height(tree->left), binary_tree_height(tree->right)) + 1);
+	return (height(binary_tree_height(tree->left),
+				binary_tree_height(tree->right)) + 1);
 }
 
 /**
